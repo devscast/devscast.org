6a670d3ff6add0f37e70ed2668711c1c24feb994
bernard-ng <ngandubernard@gmail.com>
2022-03-07 00:20:05 +0200

    fix #4 setup tailwindcss v3

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
+const tailwindcss = require('tailwindcss');
+
 module.exports = {
     plugins: [
+        tailwindcss('./tailwind.config.js'),
         require('autoprefixer'),
         require('postcss-import')
     ]
