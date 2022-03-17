55df2901dfa4f9dc31dc2a914ca6601b7458bff1
bernard-ng <ngandubernard@gmail.com>
2022-03-17 16:33:54 +0200

    #6 hexagonal architecture init

--- public/index.php
+++ public/index.php
@@ -2,10 +2,13 @@
 
 declare(strict_types=1);
 
-use App\Kernel;
+use Infrastructure\Shared\Symfony\Kernel;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel(
+        environment: $context['APP_ENV'],
+        debug: (bool)$context['APP_DEBUG']
+    );
 };
--- src/Infrastructure/Shared/Symfony/Kernel.php
+++ src/Infrastructure/Shared/Symfony/Kernel.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Infrastructure\Symfony;
+namespace Infrastructure\Shared\Symfony;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
