7242f2456f7792c76000668d324cde2efbceebd9
bernard-ng <ngandubernard@gmail.com>
2022-03-31 20:30:08 +0200

    feat(auth): setup userchecker and auth entry point

---
+++ src/Infrastructure/Authentication/Symfony/Security/AccessDeniedHandler.php
@@ -0,0 +1,40 @@
+<?php /** @noinspection PhpUnhandledExceptionInspection */
+
+declare(strict_types=1);
+
+namespace Infrastructure\Authentication\Symfony\Security;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Twig\Environment;
+
+/**
+ * Class AccessDeniedHandler
+ * @package Infrastructure\Authentication\Symfony\Security
+ * @author bernard-ng <bernard@devscast.tech>
+ */
+final class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function __construct(
+        private readonly Environment $twig
+    ) {
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): Response
+    {
+        $attributes = $accessDeniedException->getAttributes();
+
+        if (in_array('application/json', $request->getAcceptableContentTypes())) {
+            return new JsonResponse(null, Response::HTTP_FORBIDDEN);
+        }
+
+        return new Response(
+            content: $this->twig->render('bundles/TwigBundle/Exception/error403.html.twig'),
+            status: Response::HTTP_FORBIDDEN
+        );
+    }
+}
