fcce38c9ce75ca2453d9d588a9b1cc5635e1c93f
bernard-ng <ngandubernard@gmail.com>
2022-03-31 20:30:08 +0200

    feat(auth): setup userchecker and auth entry point

---
+++ src/Infrastructure/Authentication/Symfony/Security/AuthenticationEntryPoint.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infrastructure\Authentication\Symfony\Security;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\RememberMeToken;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\InsufficientAuthenticationException;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+/**
+ * Because the security component is a bit stupid and does not return an AccessDenied for users connected with a cookie
+ * We redirect the treatment of this situation to the AccessDeniedHandler
+ *
+ * Class AuthenticationEntryPoint
+ *
+ * @author bernard-ng <bernard@devscast.tech>
+ */
+final class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
+{
+    public function __construct(
+        private readonly RouterInterface $router,
+        private readonly AccessDeniedHandler $accessDeniedHandler
+    ) {
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null): Response
+    {
+        $previous = $authException?->getPrevious();
+
+        if (
+            $authException instanceof InsufficientAuthenticationException &&
+            $previous instanceof AccessDeniedException &&
+            $authException->getToken() instanceof RememberMeToken
+        ) {
+            return $this->accessDeniedHandler->handle($request, $previous);
+        }
+
+        if (in_array('application/json', $request->getAcceptableContentTypes())) {
+            return new JsonResponse(
+                data: ['title' => "Vous n'avez pas les permissions suffisantes pour effectuer cette action"],
+                status: Response::HTTP_FORBIDDEN
+            );
+        }
+
+        return new RedirectResponse($this->router->generate('authentication_login'));
+    }
+}
---
+++ src/Infrastructure/Authentication/Symfony/Security/UserChecker.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infrastructure\Authentication\Symfony\Security;
+
+use Domain\Authentication\Entity\User;
+use Domain\Authentication\Event\LoginAttemptsLimitReachedEvent;
+use Domain\Authentication\Service\LoginAttemptService;
+use Infrastructure\Authentication\Exception\TooManyLoginAttemptsException;
+use Infrastructure\Authentication\Exception\UserBannedException;
+use Infrastructure\Authentication\Exception\UserNotFoundException;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Class UserChecker
+ *
+ * @author bernard-ng <bernard@devscast.tech>
+ */
+final class UserChecker implements UserCheckerInterface
+{
+    public function __construct(
+        private readonly LoginAttemptService $loginAttemptService,
+        private readonly EventDispatcherInterface $dispatcher
+    ) {
+    }
+
+    public function checkPreAuth(UserInterface|User $user): void
+    {
+        if ($user instanceof User && $this->loginAttemptService->limitReachedFor($user)) {
+            $this->dispatcher->dispatch(new LoginAttemptsLimitReachedEvent($user));
+            throw new TooManyLoginAttemptsException();
+        }
+    }
+
+    public function checkPostAuth(UserInterface|User $user): void
+    {
+        if ($user instanceof User && $user->isBanned()) {
+            throw new UserBannedException();
+        }
+
+        if ($user instanceof User && false === $user->isConfirmed()) {
+            throw new UserNotFoundException();
+        }
+    }
+}
