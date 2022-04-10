ebc3d3d4ddaf44d6b121fc19e5d921ba63a022dc
bernard-ng <ngandubernard@gmail.com>
2022-04-10 02:15:52 +0200

    feat(trans): add translation keys and domain for authentication

--- src/Application/Authentication/Command/LoginCommand.php
+++ src/Application/Authentication/Command/LoginCommand.php
@@ -4,23 +4,15 @@ declare(strict_types=1);
 
 namespace Application\Authentication\Command;
 
-use Symfony\Component\Validator\Constraints as Assert;
-
 /**
- * Class LoginUserCommand
- * @package Application\Authentication\Command
+ * Class LoginUserCommand.
+ *
  * @author bernard-ng <bernard@devscast.tech>
  */
 final class LoginUserCommand
 {
     public function __construct(
-        #[Assert\NotBlank]
-        #[Assert\Email]
         public ?string $identifier = null,
-
-        #[Assert\NotBlank]
-        #[Assert\NotCompromisedPassword]
-        #[Assert\Length(min: 6, max: 4096)]
         public ?string $password = null
     ) {
     }
