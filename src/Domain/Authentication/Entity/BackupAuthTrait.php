a83c2da3bfee5f43d6e13cfdc57313c6e814e770
bernard-ng <ngandubernard@gmail.com>
2022-04-18 09:50:55 +0200

    feat(auth): implements backup code

--- src/Domain/Authentication/Entity/BackupAuthTrait.php
+++ src/Domain/Authentication/Entity/BackupAuthTrait.php
@@ -11,33 +11,37 @@ namespace Domain\Authentication\Entity;
  */
 trait BackupAuthTrait
 {
-    private array $backup_codes = [];
+    private ?array $backup_codes = [];
 
     public function isBackupCode(string $code): bool
     {
-        return in_array($code, $this->backup_codes, true);
+        return in_array($code, $this->getBackupCode(), true);
     }
 
-    public function setBackupCode(array $codes): self
+    public function setBackupCode(?array $codes = []): self
     {
-        if (6 !== count($codes)) {
+        if (null === $codes) {
+            $this->backup_codes = [];
+        } elseif (6 !== count($codes)) {
             throw new \InvalidArgumentException('Please set 6 backup codes with 6 digits long');
+        } else {
+            $this->backup_codes = $codes;
         }
-        $this->backup_codes = $codes;
 
         return $this;
     }
 
     public function getBackupCode(): array
     {
-        return $this->backup_codes;
+        return $this->backup_codes ?? [];
     }
 
     public function invalidateBackupCode(string $code): void
     {
+        $backup_code = $this->getBackupCode();
         $this->backup_codes = array_splice(
-            array: $this->backup_codes,
-            offset: (int) array_search($code, $this->backup_codes, true),
+            array: $backup_code,
+            offset: (int) array_search($code, $backup_code, true),
             length: 1
         );
     }
