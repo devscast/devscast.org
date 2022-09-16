afa3fb91d9df2da89f2f29c1562a8ff5cff0d9fd
bernard-ng <ngandubernard@gmail.com>
2022-09-16 16:36:40 +0200

    fix(asset): add toast custom element

--- assets/app/index.js
+++ assets/app/index.js
@@ -3,3 +3,9 @@ import './scss/app.scss'
 
 // start stimulus app
 import './js/bootstrap'
+
+// custom elements definition
+import Toast from "../shared/js/elements/Toast";
+
+// Custom Element
+customElements.define('app-toast', Toast)
