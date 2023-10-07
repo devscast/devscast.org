cc673ae5d3238a5a5420c187852c331a75e6ba3d
bernard-ng <ngandubernard@gmail.com>
2023-10-07 23:17:02 +0200

    style(lint): ecs lint

--- src/Infrastructure/Shared/Symfony/Controller/MainController.php
+++ src/Infrastructure/Shared/Symfony/Controller/MainController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Infrastructure\Shared\Symfony\Controller;
 
-use Domain\Authentication\Entity\User;
 use Devscast\Bundle\DddBundle\Infrastructure\Symfony\Controller\AbstractController;
 use Domain\Content\Repository\Blog\PostRepositoryInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,11 +21,10 @@ final class MainController extends AbstractController
     #[Route('/', name: 'app_index', methods: ['GET'])]
     public function index(PostRepositoryInterface $repository): Response
     {
-        /** @var User $user */
         $user = $this->getUser();
 
         return $this->render(
-            view: null !== $user ? '@app/domain/home_logged.html.twig' : '@app/domain/home.html.twig' ,
+            view: null !== $user ? '@app/domain/home_logged.html.twig' : '@app/domain/home.html.twig',
             parameters: [
                 'posts' => $repository->findBy([], limit: 5),
             ]
